3c27d6e5e4111de60bc2640301591a423bd17c2d
Bradley Treweek <cmd430@gmail.com>
2019-04-29 15:15:54 +1200

    change warning string

--- src/store/index.js
+++ src/store/index.js
@@ -22,7 +22,7 @@ export default new Vuex.Store({
     },
     strings: {
       importing: 'Importing',
-      downloadWarning: 'Check Download',
+      downloadWarning: 'Requires Attention', // 'Check Download',
       downloaded: 'In Plex',
       downloading: 'Downloading',
       pending: 'Pending',
