fca1127505ca0e5363989529acce24e8105855c4
Bradley Treweek <cmd430@gmail.com>
2019-05-04 21:42:04 +1200

    update history and recent

--- public/php/Home/recent.php
+++ public/php/Home/recent.php
@@ -1,66 +1,46 @@
 <?php
 
-  include "../Config/conf.php";
-  header("Content-Type: application/json");
+  require "../Config/conf.php";
 
-  $COUNT = 9;
+  $CONFIG = new Config();
 
-  $RECENT_ADDED_SHOWS = json_decode(file_get_contents("${TAUTULLI}?apikey=${API_KEY_TAUTULLI}&cmd=get_recently_added&count=${COUNT}&type=show"), true);
-  $RECENT_ADDED_MOVIES = json_decode(file_get_contents("${TAUTULLI}?apikey=${API_KEY_TAUTULLI}&cmd=get_recently_added&count=${COUNT}&type=movie"), true);
+  $LIMIT = 9;
+  $RECENT_ADDED_SHOWS = $CONFIG->Tautulli("get_recently_added", "count={$LIMIT}&type=show");
+  $RECENT_ADDED_MOVIES = $CONFIG->Tautulli("get_recently_added", "count={$LIMIT}&type=movie");
+  $RECENT_ITEMS = [];
 
-  $RECENT_ITEMS = array();
-
-  foreach ($RECENT_ADDED_SHOWS['response']['data']['recently_added'] as $item) {
-    $newItem = array();
-    if ($item['media_type'] == "episode") {
-      $newItem['title'] = $item['grandparent_title'];
-      $season = $item['parent_media_index'];
-      $episode = $item['media_index'];
-      if (strlen($season) == 1) {
-        $newItem['series']['season'] = '0' . $season;
-      } else {
-        $newItem['series']['season'] = $season;
-      }
-      if (strlen($episode) == 1) {
-        $newItem['series']['episode'] = '0' . $episode;
-      } else {
-        $newItem['series']['episode'] = $episode;
-      }
-      $newItem['series']['episode_title'] = $item['title'];
-      $RATING_KEY = $item['grandparent_rating_key'];
-      $newItem['image'] = "${IMAGE_PROXY}?rating_key=${RATING_KEY}&type=thumb";
-    } else if ($item['media_type'] == 'series') {
-      // THIS IS CURRENTLY NEVER USED!
-      $newItem['title'] = $item['parent_title'];
-      $newItem['series']['season'] = $item['media_index'];
-      $RATING_KEY = $item['rating_key'];
-      $newItem['image'] = "${IMAGE_PROXY}?rating_key=${RATING_KEY}&type=thumb";
-    } else {
-      continue;
+  foreach ($RECENT_ADDED_SHOWS['response']['data']['recently_added'] as $SERIES_RAW) {
+    if ($SERIES_RAW['media_type'] === "episode") {
+      $RECENT_ITEMS[] = [
+        'title' => $SERIES_RAW['title'],
+        'series' => [
+          'title' => $SERIES_RAW['grandparent_title'],
+          'season' => (strlen($SERIES_RAW['parent_media_index']) > 1 ? $SERIES_RAW['parent_media_index'] : "0" . $SERIES_RAW['parent_media_index']),
+          'episode' => (strlen($SERIES_RAW['media_index']) > 1 ? $SERIES_RAW['media_index'] : "0" . $SERIES_RAW['media_index']),
+          'poster' => $CONFIG->Proxy("rating_key={$SERIES_RAW['grandparent_rating_key']}&type=thumb")
+        ],
+        'mediatype' => 'episode',
+        'added' => $SERIES_RAW['added_at']
+      ];
     }
-    $newItem['media_type'] = $item['media_type'];
-    $newItem['added_at'] = $item['added_at'];
-    array_push($RECENT_ITEMS, $newItem);
   }
-  foreach ($RECENT_ADDED_MOVIES['response']['data']['recently_added'] as $item) {
-    $newItem = array();
-    if ($item['media_type'] == "movie") {
-      $newItem['title'] = $item['full_title'];
-    } else {
-      continue;
+  foreach ($RECENT_ADDED_MOVIES['response']['data']['recently_added'] as $MOVIE_RAW) {
+    if ($MOVIE_RAW['media_type'] == "movie") {
+      $RECENT_ITEMS[] = [
+        'title' => $MOVIE_RAW['full_title'],
+        'poster' => $CONFIG->Proxy("rating_key={$MOVIE_RAW['rating_key']}&type=thumb"),
+        'mediatype' => 'movie',
+        'added' => $MOVIE_RAW['added_at']
+      ];
     }
-    $newItem['media_type'] = $item['media_type'];
-    $RATING_KEY = $item['rating_key'];
-    $newItem['image'] = "${IMAGE_PROXY}?rating_key=${RATING_KEY}&type=thumb";
-    $newItem['added_at'] = $item['added_at'];
-    array_push($RECENT_ITEMS, $newItem);
   }
 
   usort($RECENT_ITEMS, function ($a, $b) {
-    if ($a['added_at'] == $b['added_at']) return 0;
-    return ($a['added_at'] > $b['added_at']) ? -1 : 1;
+    if ($a['added'] === $b['added']) return 0;
+    return ($a['added'] > $b['added']) ? -1 : 1;
   });
 
-  echo json_encode(array_slice($RECENT_ITEMS, 0, $COUNT));
+  header("Content-Type: application/json");
+  echo json_encode(array_slice($RECENT_ITEMS, 0, $LIMIT));
 
 ?>
