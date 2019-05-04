9363dc1001eb5907a7fb698f7c0820683eded8b0
Bradley Treweek <cmd430@gmail.com>
2019-05-05 00:02:07 +1200

    add vue-momentjs for formatting times

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import mixins from './mixins'
+import moment from 'vue-moment'
 
 Vue.config.devtools = true
 Vue.config.productionTip = false
 
+Vue.use(moment)
 Vue.use(mixins)
 
 new Vue({
