b20c717fb7474e2fab4436d6d5f4f25a469ccee4
Bradley Treweek <cmd430@gmail.com>
2019-05-04 18:28:02 +1200

    update image proxy to new backend

--- public/php/Shared/image.php
+++ public/php/Shared/image.php
@@ -1,11 +1,12 @@
 <?php
 
-  include "../Config/conf.php";
-  header("Content-Type: image/jpeg;");
+  require "../Config/conf.php";
+
+  $CONFIG = new Config();
 
   if (isset($_GET['tmdb_id'])) {
     $TMDB_ID = $_GET['tmdb_id'];
-    $TMDB_JSON = json_decode(file_get_contents("https://api.themoviedb.org/3/movie/${TMDB_ID}?api_key=${API_KEY_TMDB}"), true);
+    $TMDB_JSON = json_decode(file_get_contents("https://api.themoviedb.org/3/movie/{$TMDB_ID}?api_key=" . $CONFIG::TMDB_API_KEY), true);
     $POSTER_PATH = $TMDB_JSON['poster_path'];
     $IMAGE_URL = "http://image.tmdb.org/t/p/w200${POSTER_PATH}";
     $IMAGE = file_get_contents($IMAGE_URL);
@@ -17,10 +18,11 @@
     } else {
       $OPTIONS = "&height=270&fallback=thumb";
     }
-    $IMAGE_URL = "${TAUTULLI}?apikey=${API_KEY_TAUTULLI}&cmd=pms_image_proxy&img=/library/metadata/${RATING_KEY}/${TYPE}${OPTIONS}";
+    $IMAGE_URL = $CONFIG->Tautulli("pms_image_proxy", "img=/library/metadata/${RATING_KEY}/${TYPE}${OPTIONS}");
     $IMAGE = file_get_contents($IMAGE_URL);
   }
 
+  header("Content-Type: image/jpeg;");
   echo $IMAGE;
 
 ?>
