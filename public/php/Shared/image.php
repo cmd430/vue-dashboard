5f182302744c7c770d6061097aaf78e894e837f4
Bradley Treweek <cmd430@gmail.com>
2019-05-04 21:45:04 +1200

    correct image proxy ${} -> {$}

--- public/php/Shared/image.php
+++ public/php/Shared/image.php
@@ -8,7 +8,7 @@
     $TMDB_ID = $_GET['tmdb_id'];
     $TMDB_JSON = json_decode(file_get_contents("https://api.themoviedb.org/3/movie/{$TMDB_ID}?api_key=" . $CONFIG::TMDB_API_KEY), true);
     $POSTER_PATH = $TMDB_JSON['poster_path'];
-    $IMAGE_URL = "http://image.tmdb.org/t/p/w200${POSTER_PATH}";
+    $IMAGE_URL = "http://image.tmdb.org/t/p/w200{$POSTER_PATH}";
     $IMAGE = file_get_contents($IMAGE_URL);
   } else if (isset($_GET['rating_key']) && isset($_GET['type'])) {
     $TYPE = $_GET['type'];
@@ -18,7 +18,7 @@
     } else {
       $OPTIONS = "&height=270&fallback=thumb";
     }
-    $IMAGE_URL = $CONFIG->Tautulli("pms_image_proxy", "img=/library/metadata/${RATING_KEY}/${TYPE}${OPTIONS}", false);
+    $IMAGE_URL = $CONFIG->Tautulli("pms_image_proxy", "img=/library/metadata/{$RATING_KEY}/{$TYPE}{$OPTIONS}", false);
     $IMAGE = file_get_contents($IMAGE_URL);
   }
 
