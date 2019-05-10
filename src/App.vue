463b27ada70f74363884584da7b9a80a84cd50c0
Bradley Treweek <cmd430@gmail.com>
2019-05-10 22:04:26 +1200

    move icon font css to App (Global)

--- src/App.vue
+++ src/App.vue
@@ -63,4 +63,70 @@ body {
     }
   }
 }
+
+@font-face {
+  font-family: 'icons';
+  src: url('~@/assets/icons.eot?83491505');
+  src: url('~@/assets/icons.woff2?83491505') format('woff2'),
+       url('~@/assets/icons.woff?83491505') format('woff'),
+       url('~@/assets/icons.ttf?83491505') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(359deg);
+  }
+}
+
+[class^="icon-"] {
+  &::before {
+    font-family: "icons";
+    font-style: normal;
+    font-weight: normal;
+    display: inline-block;
+    text-decoration: inherit;
+    width: 1em;
+    margin-right: .2em;
+    text-align: center;
+    font-variant: normal;
+    text-transform: none;
+    line-height: 1em;
+  }
+  &.spin {
+    animation: spin 2s infinite linear;
+    display: inline-block;
+  }
+}
+.icon-menu::before { content: '\e800'; }
+.icon-th::before { content: '\e801'; }
+.icon-hourglass::before { content: '\e802'; }
+.icon-th-large::before { content: '\e803'; }
+.icon-th-list::before { content: '\e804'; }
+.icon-reload::before { content: '\e805'; }
+.icon-upload::before { content: '\e806'; }
+.icon-download::before { content: '\e807'; }
+.icon-home::before { content: '\e808'; }
+.icon-play::before { content: '\e80c'; }
+.icon-pause::before { content: '\e80d'; }
+.icon-warning::before { content: '\e80e'; }
+.icon-calendar::before { content: '\e80f'; }
+.icon-attention::before { content: '\e810'; }
+.icon-cog::before { content: '\e813'; }
+.icon-cancel::before { content: '\e816'; }
+.icon-ok::before { content: '\e817'; }
+.icon-info::before { content: '\e818'; }
+.icon-chart-pie::before { content: '\e81a'; }
+.icon-clock::before { content: '\e820'; }
+.icon-reloading::before { content: '\e832'; }
+.icon-down-open::before { content: '\f004'; }
+.icon-up::before { content: '\f005'; }
+.icon-right::before { content: '\f006'; }
+.icon-left::before { content: '\f007'; }
+.icon-github::before { content: '\f09b'; }
+.icon-lock::before { content: '\f512'; }
+.icon-unlock::before { content: '\f513'; }
 </style>
