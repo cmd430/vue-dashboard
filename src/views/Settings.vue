04231898bed2dfc8d5b0afb7b10889d9fad386c9
Bradley Treweek <cmd430@gmail.com>
2019-05-11 16:05:30 +1200

    imporove settings css

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,71 +1,68 @@
 <template>
   <div id="settings">
     <h1>Settings</h1>
-    <section id="home" class="left">
-      <h2>Home Settings</h2>
-      <div class="checkbox">
-        <label>
-          Show Activity
-          <input type="checkbox" v-model="showActivity" />
-          <span></span>
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          Show Recently Watched
-          <input type="checkbox" v-model="showHistory" />
-          <span></span>
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          Show Recently Added
-          <input type="checkbox" v-model="showRecent" />
-          <span></span>
-        </label>
-      </div>
-    </section>
-    <section id="calendar" class="center">
-      <h2>Calendar Settings</h2>
-      <div class="checkbox">
-        <label>
-          Hide Episodes and Movies that are 'In Plex'
-          <input type="checkbox" v-model="hideDownloaded" />
-          <span></span>
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          Only Show Next Unaired Episode
-          <input type="checkbox" v-model="showNextUnairedOnly" />
-          <span></span>
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          Always Show Todays Episodes
-          <input type="checkbox" v-model="alwaysShowToday" />
-          <span></span>
-        </label>
-      </div>
-    </section>
-    <section id="queue" class="right">
-      <h2>Queue Settings</h2>
-      <p>No Settings</p>
-    </section>
+    <div class="wrapper">
+      <section id="home">
+        <h2>Home Settings</h2>
+        <div class="checkbox">
+          <label>
+            Show Activity
+            <input type="checkbox" v-model="showActivity" />
+            <span></span>
+          </label>
+        </div>
+        <div class="checkbox">
+          <label>
+            Show Recently Watched
+            <input type="checkbox" v-model="showHistory" />
+            <span></span>
+          </label>
+        </div>
+        <div class="checkbox">
+          <label>
+            Show Recently Added
+            <input type="checkbox" v-model="showRecent" />
+            <span></span>
+          </label>
+        </div>
+      </section>
+      <section id="calendar">
+        <h2>Calendar Settings</h2>
+        <div class="checkbox">
+          <label>
+            Hide Episodes and Movies that are 'In Plex'
+            <input type="checkbox" v-model="hideDownloaded" />
+            <span></span>
+          </label>
+        </div>
+        <div class="checkbox">
+          <label>
+            Only Show Next Unaired Episode
+            <input type="checkbox" v-model="showNextUnairedOnly" />
+            <span></span>
+          </label>
+        </div>
+        <div class="checkbox">
+          <label>
+            Always Show Todays Episodes
+            <input type="checkbox" v-model="alwaysShowToday" />
+            <span></span>
+          </label>
+        </div>
+      </section>
+      <section id="queue">
+        <h2>Queue Settings</h2>
+        <p>No Settings</p>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Settings',
-  metaInfo: {
+  meta: {
     title: 'Settings'
-  },
-  components: {},
-  methods: {},
-  data () {
-    return {}
   },
   computed: {
     showActivity: {
@@ -116,23 +113,71 @@ export default {
         this.$store.commit('alwaysShowToday', value)
       }
     }
-  },
-  created () {}
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-p {
-  display: table;
-  font-size: 12px;
-  user-select: none;
-  color: rgb(153, 153, 153);
-  text-align: center;
-  margin: 0;
-}
-div#settings {
-  margin-bottom: 40px;
+<style lang="scss" scoped>
+div {
+  #settings {
+    margin-bottom: 40px;
+  }
+  .wrapper {
+    padding: 0 147px;
+  }
+  .checkbox {
+    position: relative;
+    height: 20px;
+    width: 100px;
+    &:not(:last-child) {
+      margin-bottom: 10px;
+    }
+    label {
+      display: inline-block;
+      position: absolute;
+      padding-left: 25px;
+      cursor: pointer;
+      font-size: 12px;
+      user-select: none;
+      color: rgb(153, 153, 153);
+      width: 250px;
+      input[type=checkbox] {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+        &:checked ~ span::after {
+          display: block;
+        }
+      }
+      &:hover input[type=checkbox] ~ span {
+        background-color: rgba(255, 255, 255, 0.3);
+      }
+      span {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 19px;
+        width: 19px;
+        border-radius: 3px;
+        border-color: rgb(204, 204, 204);
+        color: rgb(255, 255, 255);
+        background-color: rgba(255, 255, 255, 0.25);
+        &::after {
+          content: '';
+          position: absolute;
+          display: none;
+          left: 6px;
+          top: 3px;
+          width: 4px;
+          height: 8px;
+          border: solid rgb(238, 238, 238);
+          border-width: 0 3px 3px 0;
+          transform: rotate(40deg);
+        }
+      }
+    }
+  }
 }
 section {
   width: 400px;
@@ -142,76 +187,23 @@ section {
   box-sizing: border-box;
   padding: 0 20px;
   position: relative;
-}
-section.left {
-  margin-left: 147px;
-}
-/*
-section.center {}
-section.right {}
-*/
-section::before{
-  content: '';
-  background: rgba(0, 0, 0, 0.3);
-  position: absolute;
-  top: 40px;
-  left: 0px;
-  height: calc(100% - 30px);
-  width: 100%;
-  border-radius: 4px;
-}
-div.checkbox {
-  position: relative;
-  height: 20px;
-  width: 100px;
-}
-div.checkbox:not(:last-child) {
-  margin-bottom: 10px;
-}
-label {
-  display: inline-block;
-  position: absolute;
-  padding-left: 25px;
-  cursor: pointer;
-  font-size: 12px;
-  user-select: none;
-  color: rgb(153, 153, 153);
-  width: 250px;
-}
-label input[type=checkbox] {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-}
-label span {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 19px;
-  width: 19px;
-  border-radius: 3px;
-  border-color: rgb(204, 204, 204);
-  color: rgb(255, 255, 255);
-  background-color: rgba(255, 255, 255, 0.25);
-}
-label:hover input[type=checkbox] ~ span {
-  background-color: rgba(255, 255, 255, 0.3);
-}
-span::after {
-  content: "";
-  position: absolute;
-  display: none;
-}
-label input[type=checkbox]:checked ~ span::after {
-  display: block;
-}
-label span::after {
-  left: 6px;
-  top: 3px;
-  width: 4px;
-  height: 8px;
-  border: solid rgb(238, 238, 238);
-  border-width: 0 3px 3px 0;
-  transform: rotate(40deg);
+  &::before{
+    content: '';
+    background: rgba(0, 0, 0, 0.3);
+    position: absolute;
+    top: 40px;
+    left: 0px;
+    height: calc(100% - 30px);
+    width: 100%;
+    border-radius: 4px;
+  }
+  p {
+    display: table;
+    font-size: 12px;
+    user-select: none;
+    color: rgb(153, 153, 153);
+    text-align: center;
+    margin: 0;
+  }
 }
 </style>
