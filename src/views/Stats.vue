aa99afe574eb3f754299825696bebee0c45b8fa0
Bradley Treweek <cmd430@gmail.com>
2019-05-11 16:05:04 +1200

    css -> scss

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -15,13 +15,23 @@ import Bandwidth from '@/components/Stats/Bandwidth'
 
 export default {
   name: 'Stats',
-  metaInfo: {
+  meta: {
     title: 'Stats'
   },
   components: {
     'libraries': Libraries,
     'diskspace': Diskspace,
     'bandwidth': Bandwidth
+  },
+  data () {
+    return {
+      stats: {
+        libraries: null,
+        diskspace: null,
+        bandwidth: null
+      },
+      update: null
+    }
   },
   methods: {
     showStats: function () {
@@ -40,16 +50,6 @@ export default {
         })
     }
   },
-  data () {
-    return {
-      stats: {
-        libraries: null,
-        diskspace: null,
-        bandwidth: null
-      },
-      update: null
-    }
-  },
   created () {
     this.showStats()
   },
@@ -66,7 +66,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 ul {
   display: flex;
   flex-wrap: wrap;
@@ -74,16 +74,58 @@ ul {
   justify-content: flex-start;
   padding: 0 96px;
   list-style-type: none;
+  &:empty::after {
+    content: "Nothing to Show";
+    width: 100%;
+    font-size: 13px;
+    color: rgb(153, 153, 153);
+    text-align: center;
+    padding: 15px;
+    margin: 0 27px 0 11px;
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+  }
+  &::v-deep li {
+    display: inline-block;
+    padding: 10px;
+    position: relative;
+    box-sizing: content-box;
+    section {
+      height: 135px;
+      min-width: 300px;
+      margin-right: 20px;
+      margin-bottom: 20px;
+      display: inline-block;
+      padding: 20px;
+      position: relative;
+      vertical-align: text-top;
+      &::before{
+        content: '';
+        background: rgba(0, 0, 0, 0.3);
+        position: absolute;
+        left: 0;
+        top: 0px;
+        height: 100%;
+        width: 100%;
+        border-radius: 4px;
+      }
+      div {
+        position: relative;
+        p {
+          display: table;
+          font-size: 16px;
+          user-select: none;
+          color: rgb(238, 238, 238);
+          text-align: left;
+          margin: 0;
+          span {
+            text-align: center;
+            color: rgb(153, 153, 153);
+          }
+        }
+      }
+    }
+  }
 }
-ul:empty::after {
-  content: "Nothing to Show";
-  width: 100%;
-  font-size: 13px;
-  color: rgb(153, 153, 153);
-  text-align: center;
-  padding: 15px;
-  margin: 0 27px 0 11px;
-  background: rgba(0, 0, 0, 0.1);
-  border-radius: 4px;
-}
+
 </style>
